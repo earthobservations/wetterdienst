186b9d9898e1bf18f332ee9e45f1cf0ead25c517
gezzze <gutzemann@gmail.com>
2018-12-09 23:06:23 +0100

    Introduce setup.py to make it a module and installable

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 from setuptools import setup
 
-from python_dwd import __version__
-
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-    name='python_dwd',
-    version=__version__,
-    description='A module for accessing data of the german weather service',
+    name='pydwd',
+    version=0.1,
+    description='A module for accessing data from german weather service',
     license='MIT',
     long_description=long_description,
     author='Benjamin Gutzmann',
     author_email='gutzemann@gmail.com',
-    packages=['python_dwd'],  # , 'python_dwd.additionals'
-    install_requires=['pandas', 'pathlib',
-                      'zipfile', 'scipy', 'numpy']
+    packages=['pydwd'],
+    install_requires=['pandas']
 )
