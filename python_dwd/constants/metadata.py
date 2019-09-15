9171dc0446a0906798f82b046347f38b86035678
meteoDaniel <daniel.lassahn@gmail.com>
2019-09-15 21:03:43 +0200

    Refactoring

--- python_dwd/constants/metadata.py
+++ python_dwd/constants/metadata.py
@@ -15,6 +15,5 @@ METADATA_NAME = 'metadata'
 FTP_METADATA_NAME = "meta_data"
 ARCHIVE_FORMAT = '.zip'
 DATA_FORMAT = '.csv'
-# Column specifications used for fixed-width-file (pandas.read_fwf)
-# All metadata files seem to have the same fixed column widths
-METADATA_COLSPECS = [(0, 5), (5, 14), (14, 23), (23, 38), (38, 50), (50, 60), (60, 102), (102, 200)]
+METADATA_FIXED_COLUMN_WIDTH = [(0, 5), (5, 14), (14, 23), (23, 38),
+                               (38, 50), (50, 60), (60, 102), (102, 200)]
