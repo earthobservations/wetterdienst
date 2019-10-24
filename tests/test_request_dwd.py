69d409c4afd7e064d0b9ff53af39fce0703057ee
Benjamin Gutzmann <gutzemann@gmail.com>
2019-10-24 22:22:36 +0200

    Implemented function for extracting a number from string and test for this and function to get station id in FuzzyExtractor

--- tests/test_request_dwd.py
+++ tests/test_request_dwd.py
@@ -1,8 +1,30 @@
 import pytest
-from python_dwd.request_dwd import DWDRequest
+from python_dwd.request_dwd import DWDRequest, extract_numbers_from_string
 from python_dwd.enumerations.parameter_enumeration import Parameter
 from python_dwd.enumerations.period_type_enumeration import PeriodType
 from python_dwd.enumerations.time_resolution_enumeration import TimeResolution
+
+
+def test_extract_numbers_from_string():
+    with pytest.raises(TypeError):
+        extract_numbers_from_string(string=1, number_type="float", decimal=".")
+
+    with pytest.raises(TypeError):
+        extract_numbers_from_string(string="abc", number_type="str", decimal=".")
+
+    with pytest.raises(ValueError):
+        extract_numbers_from_string(string="1234", number_type="int", decimal=";")
+
+    with pytest.raises(ValueError):
+        extract_numbers_from_string(string="1234.5", number_type="int", decimal=".")
+
+    assert extract_numbers_from_string(string="abc", number_type="float", decimal=".") is []
+
+    assert extract_numbers_from_string(string=" B 123 CDE 45", number_type="int", decimal="") == [123, 45]
+
+    assert extract_numbers_from_string(string=".123.", number_type="int", decimal=".") == [123]
+
+    assert extract_numbers_from_string(string=".123.", number_type="float", decimal=".") == [123.0]
 
 
 def test_station_id():
