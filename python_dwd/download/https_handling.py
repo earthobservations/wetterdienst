16dced069afa744574080e1ce0f2117ef7dc8c2b
Benjamin Gutzmann <gutzemann@gmail.com>
2020-04-28 20:06:51 +0200

    Changes to https handling

--- python_dwd/download/https_handling.py
+++ python_dwd/download/https_handling.py
@@ -7,102 +7,107 @@ from lxml.html import document_fromstring  # lxml preferred over bs4, as it is a
 from functools import partial
 from multiprocessing.dummy import Pool  # concurrent version, as parallel version does not work with sessions
 
-from python_dwd.constants.access_credentials import DWD_SERVER, HTTPS_EXPRESSION
+HTTPS_EXPRESSION = "https://"
+MAX_RETRIES = 3
 
 
-class DWDSession:
-    """ Session class used all over the library to interact with the DWD server """
-    def __init__(self,
-                 observations_path: str) -> None:
-        """
+def _create_dwd_session() -> requests.Session:
+    """
+    Function used to create a global session that is used for listing/downloading data from the DWD server.
 
-        Args:
-            observations_path: the path that relates to the service of DWD, e.g. climate observations
-        """
-        global _dwd_session
+    Returns:
+        requests.Session object that then can be used for requests to the server
+    """
+    global _dwd_session
 
-        try:
-            _dwd_session
-        except NameError:
-            _dwd_session = requests.Session()
-            _dwd_session.mount(DWD_SERVER, HTTPAdapter(max_retries=3))
+    try:
+        _dwd_session
+    except NameError:
+        _dwd_session = requests.Session()
+        _dwd_session.mount(HTTPS_EXPRESSION, HTTPAdapter(max_retries=MAX_RETRIES))
 
-        self._dwd_session = _dwd_session
-        self._dwd_server = DWD_SERVER
-        self._dwd_observations_path = observations_path
-        self._https_expression = HTTPS_EXPRESSION
+    return _dwd_session
 
-    def list_files(self,
-                   path: Union[PurePosixPath, str],
-                   also_subfolders: bool) -> List[str]:
-        """
-        A function used to create a listing of all files of a given path on the server
 
-        Args:
-            path: the path which should be searched for files
-            also_subfolders: definition if the function should iteratively list files from subfolders
+def list_files(path: Union[PurePosixPath, str],
+               base_url: Union[PurePosixPath, str],
+               also_subfolders: bool) -> List[str]:
+    """
+    A function used to create a listing of all files of a given path on the server
 
-        Returns:
-            a list of strings representing the files from the path
-        """
-        r = self._dwd_session.get(self._build_path(path))
-        r.raise_for_status()
+    Args:
+        path: the path which should be searched for files
+        base_url: the base_url representing the part of the server which is worked with
+        also_subfolders: definition if the function should iteratively list files from subfolders
 
-        soup = document_fromstring(r.text)
+    Returns:
+        a list of strings representing the files from the path
+    """
+    dwd_session = _create_dwd_session()
 
-        files_and_folders = [link.get("href") for link in soup.xpath("//a") if link.get("href") != "../"]
+    r = dwd_session.get(_build_path(path, base_url))
+    r.raise_for_status()
 
-        files = []
-        folders = []
+    soup = document_fromstring(r.text)
 
-        for f in files_and_folders:
-            if not f.endswith("/"):
-                files.append(str(PurePosixPath(remote_path, f)))
-            else:
-                folders.append(PurePosixPath(remote_path, f))
+    files_and_folders = [link.get("href") for link in soup.xpath("//a") if link.get("href") != "../"]
 
-        if also_subfolders:
-            files_in_folders = Pool().map(partial(self.list_files, also_subfolders=also_subfolders), folders)
-            for files_in_folder in files_in_folders:
-                files.extend(files_in_folder)
+    files = []
+    folders = []
 
-        return files
+    for f in files_and_folders:
+        if not f.endswith("/"):
+            files.append(str(PurePosixPath(path, f)))
+        else:
+            folders.append(PurePosixPath(path, f))
 
-    def download_file(self,
-                      filepath: str) -> BytesIO:
-        """
-        A function used to download a specified file from the server
+    if also_subfolders:
+        files_in_folders = Pool().map(partial(list_files, base_url=base_url, also_subfolders=also_subfolders), folders)
+        for files_in_folder in files_in_folders:
+            files.extend(files_in_folder)
 
-        Args:
-            filepath: the path that defines the file
+    return files
 
-        Returns:
-            bytes of the file
-        """
-        r = self._dwd_session.get(self._build_path(filepath))
-        r.raise_for_status()
 
-        return BytesIO(r.content)
+def download_file(filepath: Union[PurePosixPath, str],
+                  base_url: Union[PurePosixPath, str]) -> BytesIO:
+    """
+    A function used to download a specified file from the server
 
-    def _build_path(self, path: str) -> str:
-        """
-        A function used to create the filepath consisting of the server, the observation path and the path of a
-        subdirectory/file
+    Args:
+        filepath: the path that defines the file
+        base_url: the base_url representing the part of the server which is worked with
 
-        Args:
-            path: the path of folder/file on the server
+    Returns:
+        bytes of the file
+    """
+    dwd_session = _create_dwd_session()
 
-        Returns:
-            the path create from the given parameters
-        """
-        return f"{self._https_expression}{PurePosixPath(self._dwd_server, self._dwd_observations_path, path)}"
+    r = dwd_session.get(_build_path(filepath, base_url))
+    r.raise_for_status()
+
+    return BytesIO(r.content)
+
+
+def _build_path(path: Union[PurePosixPath, str],
+                base_url: Union[PurePosixPath, str]) -> str:
+    """
+    A function used to create the filepath consisting of the server, the observation path and the path of a
+    subdirectory/file
+
+    Args:
+        path: the path of folder/file on the server
+
+    Returns:
+        the path create from the given parameters
+    """
+    return f"{HTTPS_EXPRESSION}{PurePosixPath(base_url, path)}"
 
 
 if __name__ == "__main__":
-    dwd_obs_path = 'climate_environment/CDC/observations_germany/climate'
-    dwd_session = DWDSession(dwd_obs_path)
+    from python_dwd.constants.access_credentials import DWD_SERVER, DWD_CLIMATE_OBSERVATIONS
 
-    remote_path = "1_minute/precipitation/historical/"
-    files = dwd_session.list_files(remote_path, True)
+    path = "1_minute/precipitation/historical/"
+    files = list_files(path, PurePosixPath(DWD_SERVER) / DWD_CLIMATE_OBSERVATIONS, True)
 
     print(files)
