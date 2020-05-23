8197e33b238806ee31c2243cfe642c827421812d
Benjamin Gutzmann <gutzemann@gmail.com>
2020-05-23 12:39:55 +0200

    Add new methods organized in data collection and data storing

---
+++ python_dwd/data_storing.py
@@ -0,0 +1,23 @@
+""" Data storing/restoring methods"""
+
+
+def store_dwd_data():
+    """
+    Function to store data in local file
+    Returns:
+
+    """
+    pass
+
+
+def restore_dwd_data():
+    """
+    Function to restore data from a local file
+    Returns:
+
+    """
+    pass
+
+
+def build_local_store_key():
+    pass
