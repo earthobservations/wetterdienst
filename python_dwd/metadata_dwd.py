ccec294eb6ead1514f4705646d2635c8e35e7e0a
Benjamin Gutzmann <gutzemann@gmail.com>
2020-06-21 11:25:30 +0200

    Readd writing to file for metadata

--- python_dwd/metadata_dwd.py
+++ python_dwd/metadata_dwd.py
@@ -3,8 +3,9 @@ from pathlib import Path
 from typing import Union
 import pandas as pd
 
+from python_dwd.additionals.functions import check_parameters
 from python_dwd.additionals.helpers import metaindex_for_1minute_data, create_metaindex
-from python_dwd.constants.access_credentials import DWD_FOLDER_METADATA
+from python_dwd.constants.access_credentials import DWD_FOLDER_METADATA, DWD_FOLDER_MAIN
 from python_dwd.constants.metadata import METADATA_NAME, DATA_FORMAT
 from python_dwd.enumerations.column_names_enumeration import DWDMetaColumns
 from python_dwd.enumerations.parameter_enumeration import Parameter
@@ -12,7 +13,7 @@ from python_dwd.enumerations.period_type_enumeration import PeriodType
 from python_dwd.enumerations.time_resolution_enumeration import TimeResolution
 from python_dwd.file_path_handling.file_index_creation import create_file_index_for_dwd_server, \
     reset_file_index_cache
-from python_dwd.file_path_handling.path_handling import create_folder
+from python_dwd.file_path_handling.path_handling import create_folder, remove_old_file
 
 
 def add_filepresence(metainfo: pd.DataFrame,
@@ -43,6 +44,8 @@ def add_filepresence(metainfo: pd.DataFrame,
 def metadata_for_dwd_data(parameter: Union[Parameter, str],
                           time_resolution: Union[TimeResolution, str],
                           period_type: Union[PeriodType, str],
+                          folder: str = DWD_FOLDER_MAIN,
+                          write_file: bool = True,
                           create_new_file_index: bool = False) -> pd.DataFrame:
     """
     A main function to retrieve metadata for a set of parameters that creates a
@@ -56,6 +59,8 @@ def metadata_for_dwd_data(parameter: Union[Parameter, str],
         parameter: observation measure
         time_resolution: frequency/granularity of measurement interval
         period_type: recent or historical files
+        folder: local file system folder where files should be stored
+        write_file: writes the meta data file to the local file system
         create_new_file_index: if true: a new file_list for metadata will
          be created
     Returns:
@@ -67,6 +72,15 @@ def metadata_for_dwd_data(parameter: Union[Parameter, str],
     parameter = Parameter(parameter)
     time_resolution = TimeResolution(time_resolution)
     period_type = PeriodType(period_type)
+
+    check_parameters(parameter=parameter,
+                     time_resolution=time_resolution,
+                     period_type=period_type)
+
+    file_path = create_metainfo_fpath(folder,
+                                      parameter,
+                                      period_type,
+                                      time_resolution)
 
     if time_resolution == TimeResolution.MINUTE_1:
         metainfo = metaindex_for_1minute_data(parameter=parameter,
@@ -93,6 +107,19 @@ def metadata_for_dwd_data(parameter: Union[Parameter, str],
                                 parameter=parameter,
                                 time_resolution=time_resolution,
                                 period_type=period_type)
+
+    if write_file and not file_path.is_file() and create_new_file_index:
+        remove_old_file(file_type=METADATA_NAME,
+                        file_postfix=DATA_FORMAT,
+                        parameter=parameter,
+                        time_resolution=time_resolution,
+                        period_type=period_type,
+                        folder=folder,
+                        subfolder=DWD_FOLDER_METADATA)
+
+        metainfo.to_csv(path_or_buf=file_path,
+                        header=True,
+                        index=False)
 
     return metainfo
 
