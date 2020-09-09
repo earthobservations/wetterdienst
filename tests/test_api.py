e8656fd1c12fa58fb8acb3d4741b88ee139cb594
Benjamin Gutzmann <gutzemann@gmail.com>
2020-09-09 23:44:11 +0200

    Remove unnecessary functions, reorder tests for api module, add notice for contribution

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,18 @@
+from datetime import datetime
+from io import BytesIO
+from pathlib import Path
+
 import pytest
 import pandas as pd
 
-from wetterdienst.api import DWDStationRequest
+from wetterdienst.api import DWDStationRequest, DWDRadolanRequest
 from wetterdienst.exceptions import StartDateEndDateError
 from wetterdienst.enumerations.parameter_enumeration import Parameter
 from wetterdienst.enumerations.period_type_enumeration import PeriodType
 from wetterdienst.enumerations.time_resolution_enumeration import TimeResolution
+
+
+FIXTURES_DIR = Path(__file__).parent / "fixtures"
 
 
 def test_dwd_station_request():
@@ -13,12 +20,12 @@ def test_dwd_station_request():
         station_ids=[1],
         parameter="kl",
         time_resolution="daily",
-        period_type="historical",
+        period_type=["recent", "historical"],
     ) == [
         [1],
         [Parameter.CLIMATE_SUMMARY],
         TimeResolution.DAILY,
-        [PeriodType.HISTORICAL],
+        [PeriodType.HISTORICAL, PeriodType.RECENT],  # check order
         None,
         None,
     ]
@@ -37,8 +44,7 @@ def test_dwd_station_request():
         None,
     ]
 
-
-def test_station_id():
+    # station id
     with pytest.raises(ValueError):
         DWDStationRequest(
             station_ids="test",
@@ -47,8 +53,7 @@ def test_station_id():
             time_resolution=TimeResolution.DAILY,
         )
 
-
-def test_time_input():
+    # time input
     assert DWDStationRequest(
         station_ids=[1],
         parameter=Parameter.CLIMATE_SUMMARY,
@@ -100,3 +105,26 @@ def test_time_input():
             start_date="1971-01-01",
             end_date="1951-01-01",
         )
+
+
+def test_dwd_radolan_request():
+    with pytest.raises(ValueError):
+        DWDRadolanRequest(
+            time_resolution=TimeResolution.MINUTE_1, date_times=["2019-08-08 00:50:00"]
+        )
+
+    request = DWDRadolanRequest(
+        time_resolution=TimeResolution.HOURLY, date_times=["2019-08-08 00:50:00"]
+    )
+
+    assert request == DWDRadolanRequest(
+        TimeResolution.HOURLY,
+        [datetime(year=2019, month=8, day=8, hour=0, minute=50, second=0)],
+    )
+
+    with Path(FIXTURES_DIR, "radolan_hourly_201908080050").open("rb") as f:
+        radolan_hourly = BytesIO(f.read())
+
+    radolan_hourly_test = next(request.collect_data())[1]
+
+    assert radolan_hourly.getvalue() == radolan_hourly_test.getvalue()
