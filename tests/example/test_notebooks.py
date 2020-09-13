fa8786465142915b3301babac9f6470dd3ea306b
Benjamin Gutzmann <gutzemann@gmail.com>
2020-09-13 03:42:26 +0200

    Fix coverage installations one more time

--- tests/example/test_notebooks.py
+++ tests/example/test_notebooks.py
@@ -5,11 +5,8 @@ from pytest_notebook.nb_regression import NBRegressionFixture
 EXAMPLE_DIR = Path(__file__).parent.parent.parent / "example"
 
 FIXTURE = NBRegressionFixture(
-    exec_timeout=50,
-    diff_ignore=("/*", ),  # ignore different outputs as long as notebook runs
-    force_regen=True,
-    coverage=False,
-    diff_color_words=False,
+    diff_ignore=(),
+    force_regen=True
 )
 
 
