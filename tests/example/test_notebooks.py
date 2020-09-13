f29a8afc2d733453c5b7afcad9ee0317d23e7bd2
Benjamin Gutzmann <gutzemann@gmail.com>
2020-09-13 09:52:43 +0200

    Fix coverage installations one more time

--- tests/example/test_notebooks.py
+++ tests/example/test_notebooks.py
@@ -5,7 +5,10 @@ from pytest_notebook.nb_regression import NBRegressionFixture
 EXAMPLE_DIR = Path(__file__).parent.parent.parent / "example"
 
 FIXTURE = NBRegressionFixture(
-    diff_ignore=(),
+    diff_ignore=(
+        "/metadata/language_info",  # Python version depends on testing
+        "/cells/*/outputs/*/data/image/png"  # pictures have random hashes
+    ),
     force_regen=True
 )
 
