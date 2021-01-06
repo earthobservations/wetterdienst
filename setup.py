9df6d1c7609eb6c97dcb1829aca412f271882f59
Andreas Motl <andreas.motl@panodata.org>
2021-01-06 13:56:18 +0100

    Add "setup.py" shim to make GitHub detect this as a Python package
    Hopefully, this will establish the "Used by" widget.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# This is a shim to hopefully allow GitHub to detect the package.
+# The build is done with Poetry.
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(name="wetterdienst")
