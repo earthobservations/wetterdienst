fa29a0f38c803bd0e544b32f7a5efed657f4c437
Andreas Motl <andreas.motl@panodata.org>
2021-03-29 23:50:22 +0200

    Fix build and improve Docker image testing
    Along the lines, bump to Python 3.8.8 within Docker containers.

---
+++ .github/release/selftest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+flavor=$1
+
+echo "Testing wetterdienst"
+wetterdienst --version
+wetterdienst dwd about parameters
+
+if [[ ${flavor} = "full" ]]; then
+  echo "Checking libraries"
+  python -c 'import gdal; print("gdal:", gdal.VersionInfo())'
+  python -c 'import wradlib; print("wradlib:", wradlib.__version__)'
+fi
