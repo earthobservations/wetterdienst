d691dcb3c4bad169d26c5e912e43c0f140aadd1a
Andreas Motl <andreas.motl@panodata.org>
2021-04-02 00:30:44 +0200

    UI: Improve test suite

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,9 +1,55 @@
 import pytest
 from dash.testing.application_runners import import_app
+from dash.testing.composite import DashComposite
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def dash_tre(request, dash_thread_server, tmpdir):
+    with DashCompositePlus(
+        dash_thread_server,
+        browser=request.config.getoption("webdriver"),
+        remote=request.config.getoption("remote"),
+        remote_url=request.config.getoption("remote_url"),
+        headless=request.config.getoption("headless"),
+        options=request.config.hook.pytest_setup_options(),
+        download_path=tmpdir.mkdir("download").strpath,
+        percy_assets_root=request.config.getoption("percy_assets"),
+        percy_finalize=request.config.getoption("nopercyfinalize"),
+        pause=request.config.getoption("pause"),
+    ) as dc:
+        yield dc
+
+
+class DashCompositePlus(DashComposite):
+    """
+    Improve vanilla library with some convenience event handlers.
+    """
+
+    def wait_for_element_by_id_clickable(self, element_id, timeout=None):
+        """
+        Explicit wait until the element is clickable.
+
+        Derived from `dash.testing.browser::wait_for_element_by_id()` and
+        https://stackoverflow.com/q/56085152.
+        """
+        return self._wait_for(
+            EC.element_to_be_clickable,
+            ((By.ID, element_id),),
+            timeout,
+            "timeout {}s => waiting for element id {}".format(
+                timeout if timeout else self._wait_timeout, element_id
+            ),
+        )
 
 
 @pytest.fixture(scope="function")
-def wetterdienst_ui(dash_duo):
+def wetterdienst_ui(dash_tre):
+
+    # Import Dash application in testing mode.
     app = import_app("wetterdienst.ui.app")
-    dash_duo.start_server(app)
-    dash_duo.wait_for_page(timeout=10)
+
+    # Start testing server and wait until page is loaded.
+    dash_tre.start_server(app)
+    dash_tre.wait_for_page(timeout=10)
--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -22,31 +22,34 @@ from bs4 import BeautifulSoup
 
 @pytest.mark.slow
 @pytest.mark.ui
-def test_app_layout(wetterdienst_ui, dash_duo):
+def test_app_layout(wetterdienst_ui, dash_tre):
 
     # Sanity check if we are on the right page.
-    assert dash_duo.find_element("h1").text == "Wetterdienst UI"
+    assert dash_tre.find_element("h1").text == "Wetterdienst UI"
 
     # Roughly verify the application elements.
-    assert dash_duo.find_element("#navigation")
-    assert dash_duo.find_element("#map")
-    assert dash_duo.find_element("#graph")
+    assert dash_tre.find_element("#navigation")
+    assert dash_tre.find_element("#map")
+    assert dash_tre.find_element("#graph")
 
 
 @pytest.mark.slow
 @pytest.mark.ui
-def test_app_data_stations(wetterdienst_ui, dash_duo):
+def test_app_data_stations(wetterdienst_ui, dash_tre):
     """
     Verify if data for "stations" has been correctly propagated.
     """
 
-    dash_duo.wait_for_element_by_id("hidden-div-metadata", timeout=5)
+    # Wait for data element.
+    dash_tre.wait_for_element_by_id("hidden-div-metadata", timeout=5)
     time.sleep(0.5)
 
-    dom: BeautifulSoup = dash_duo.dash_innerhtml_dom
-
+    # Read payload from data element.
+    dom: BeautifulSoup = dash_tre.dash_innerhtml_dom
     data_element = dom.find(attrs={"id": "hidden-div-metadata"})
     data = json.loads(data_element.text)
+
+    # Verify data.
     assert data["columns"] == [
         "station_id",
         "from_date",
@@ -57,7 +60,7 @@ def test_app_data_stations(wetterdienst_ui, dash_duo):
         "station_name",
         "state",
     ]
-    assert len(data["data"]) == 972
+    assert len(data["data"]) == 511
 
 
 @pytest.mark.xfail
