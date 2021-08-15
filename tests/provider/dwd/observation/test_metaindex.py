6d658c983bc52e910f59f3da9d7dc8f49fe2a0fd
Andreas Motl <andreas.motl@panodata.org>
2021-08-15 21:54:24 +0200

    Address review comments

--- tests/provider/dwd/observation/test_metaindex.py
+++ tests/provider/dwd/observation/test_metaindex.py
@@ -2,9 +2,7 @@
 # Copyright (c) 2018-2021, earthobservations developers.
 # Distributed under the MIT License. See LICENSE for more info.
 """ tests for file index creation """
-import aiohttp
 import pytest
-import requests
 
 from wetterdienst.metadata.columns import Columns
 from wetterdienst.metadata.period import Period
@@ -31,13 +29,7 @@ def test_meta_index_creation_success():
 @pytest.mark.remote
 def test_meta_index_creation_failure():
 
-    with pytest.raises(
-        (
-            requests.exceptions.HTTPError,
-            aiohttp.client_exceptions.ClientResponseError,
-            FileNotFoundError,
-        )
-    ):
+    with pytest.raises(FileNotFoundError):
         create_meta_index_for_climate_observations(
             DwdObservationDataset.CLIMATE_SUMMARY,
             Resolution.MINUTE_1,
