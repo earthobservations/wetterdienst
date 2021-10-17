844e926aa2e5c40231872869fe17e3314b3b587b
meteoDaniel <derArmeRitter56>
2021-10-17 17:31:27 +0200

    DRAFT: Adding first code for road weather data API

---
+++ wetterdienst/provider/dwd/road_weather/metadata/dataset.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018-2021, earthobservations developers.
+# Distributed under the MIT License. See LICENSE for more info.
+from enum import Enum
+from typing import Dict, List
+
+
+class DwdObservationRoadWeatherDataset(Enum):
+    """
+
+    enumeration for different parameter/variables
+    measured by dwd road weather stations
+    """
+
+    ROAD_SURFACE_TEMPERATURE = "roadSurfaceTemperature"
+    TEMPERATURE_AIR = "airTemperature"
+    ROAD_SURFACE_CONDITION = "roadSurfaceCondition"
+    WATER_FILM_THICKNESS = "waterFilmThickness"
+    WIND_EXTREME = "maximumWindGustSpeed"
+    WIND_EXTREME_DIRECTION = "maximumWindGustDirection"
+    WIND_DIRECTION = "windDirection"
+    WIND = "windSpeed"
+    DEW_POINT = "dewpointTemperature"
+    RELATIVE_HUMIDITY = "relativeHumidity"
+    TEMPERATURE_SOIL = "soil_temperature"
+    VISIBILITY = "visibility"
+    PRECIPITATION_TYPE = "precipitationType"
+    TOTAL_PRECIPITATION = "totalPrecipitationOrTotalWaterEquivalent"
+    INTENSITY_OF_PRECIPITATION = "intensityOfPrecipitation"
+    INTENSITY_OF_PHENOMENA = "intensityOfPhenomena"
+    HORIZONTAL_VISIBILITY = "horizontalVisibility"
+    SHORT_STATION_NAME = "shortStationName"
