574b2c51f8d5b30cdabfad001331fcdfc8998654
benjamin <gutzemann@gmail.com>
2022-02-03 00:17:47 +0100

    Update wetterdienst explorer to leverage all available weather services

--- wetterdienst/ui/explorer/app.py
+++ wetterdienst/ui/explorer/app.py
@@ -9,22 +9,15 @@ from typing import Optional
 
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go
 import requests
-from dash.dependencies import Input, Output, State
+from dash import Input, Output, State, dcc, html
 
-from wetterdienst import Settings
+from wetterdienst.api import ApiEndpoints
 from wetterdienst.exceptions import InvalidParameterCombination
 from wetterdienst.metadata.columns import Columns
-from wetterdienst.provider.dwd.observation import (
-    DwdObservationDataset,
-    DwdObservationPeriod,
-    DwdObservationRequest,
-    DwdObservationResolution,
-)
+from wetterdienst.ui.core import get_stations, get_values
 from wetterdienst.ui.explorer.layout.main import get_app_layout
 from wetterdienst.ui.explorer.library import add_annotation_no_data, default_figure
 from wetterdienst.ui.explorer.util import frame_summary
@@ -58,12 +51,15 @@ def toggle_about(n1, n2, is_open):
 @app.callback(
     Output("dataframe-stations", "children"),
     [
-        Input("select-parameter", "value"),
+        Input("select-provider", "value"),
+        Input("select-network", "value"),
         Input("select-resolution", "value"),
+        Input("select-dataset", "value"),
+        Input("select-parameter", "value"),
         Input("select-period", "value"),
     ],
 )
-def fetch_stations(parameter: str, resolution: str, period: str):
+def fetch_stations(provider: str, network: str, resolution: str, dataset: str, parameter: str, period: str):
     """
     Fetch "stations" data.
 
@@ -71,16 +67,38 @@ def fetch_stations(parameter: str, resolution: str, period: str):
 
     The data will be stored on a hidden within the browser DOM.
     """
-    log.info(f"Requesting stations for " f"parameter={parameter}, " f"resolution={resolution}, " f"period={period}")
+    if not (provider and network and resolution and dataset and parameter and period):
+        return empty_frame
+
+    api = ApiEndpoints[provider][network].value
+
+    if period == "ALL":
+        period = [*api._period_base]
+
+    log.info(f"Requesting stations for parameter={parameter}, resolution={resolution}, period={period}")
+
     try:
-        stations = DwdObservationRequest(
-            parameter=DwdObservationDataset(parameter),
-            resolution=DwdObservationResolution(resolution),
-            period=DwdObservationPeriod(period),
-        ).all()
+        stations = get_stations(
+            api=api,
+            parameter=parameter,
+            resolution=resolution,
+            period=period,
+            date=None,
+            issue="latest",
+            all_=True,
+            station_id=None,
+            name=None,
+            coordinates=None,
+            rank=None,
+            distance=None,
+            bbox=None,
+            sql=None,
+            si_units=True,
+            tidy=True,
+            humanize=True,
+        )
     except (requests.exceptions.ConnectionError, InvalidParameterCombination) as ex:
         log.warning(ex)
-        # raise PreventUpdate
         log.error("Unable to connect to data source")
         return empty_frame
 
@@ -94,13 +112,18 @@ def fetch_stations(parameter: str, resolution: str, period: str):
 @app.callback(
     Output("dataframe-values", "children"),
     [
-        Input("select-parameter", "value"),
+        Input("select-provider", "value"),
+        Input("select-network", "value"),
         Input("select-resolution", "value"),
+        Input("select-dataset", "value"),
+        Input("select-parameter", "value"),
         Input("select-period", "value"),
         Input("select-station", "value"),
     ],
 )
-def fetch_values(parameter: str, resolution: str, period: str, station_id: int):
+def fetch_values(
+    provider: str, network: str, resolution: str, dataset: str, parameter: str, period: str, station_id: int
+):
     """
     Fetch "values" data.
 
@@ -108,36 +131,51 @@ def fetch_values(parameter: str, resolution: str, period: str, station_id: int):
 
     The data will be stored on a hidden within the browser DOM.
     """
+    if not (provider and network and resolution and dataset and parameter and period):
+        return empty_frame
 
     # Sanity checks.
     if station_id is None:
         log.warning("Querying without station_id is rejected")
         return empty_frame
 
+    api = ApiEndpoints[provider][network].value
+
+    if period == "ALL":
+        period = [*api._period_base]
+
     log.info(
-        f"Requesting values for "
-        f"station_id={station_id}, "
-        f"parameter={parameter}, "
-        f"resolution={resolution}, "
+        f"Requesting values for station_id={station_id}, parameter={parameter}, resolution={resolution}, "
         f"period={period}"
     )
 
-    Settings.tidy = False
-    Settings.humanize = True
-
-    stations = DwdObservationRequest(
-        parameter=DwdObservationDataset(parameter),
-        resolution=DwdObservationResolution(resolution),
-        period=DwdObservationPeriod(period),
-    ).filter_by_station_id(station_id=(str(station_id),))
-
     try:
-        df = stations.values.all().df
+        values = get_values(
+            api=api,
+            parameter=parameter,
+            resolution=resolution,
+            period=period,
+            date=None,
+            issue="latest",
+            all_=None,
+            station_id=station_id,
+            name=None,
+            coordinates=None,
+            rank=None,
+            distance=None,
+            bbox=None,
+            sql=None,
+            sql_values=None,
+            si_units=True,
+            tidy=True,
+            humanize=True,
+        )
+        df = values.df
     except ValueError:
         log.exception("No data received")
         return empty_frame
 
-    df = df.dropna(axis=0)
+    df = df.drop(columns="quality").dropna(axis=0)
 
     log.info(f"Propagating values data frame with {frame_summary(df)}")
 
@@ -169,52 +207,37 @@ def render_navigation_stations(payload):
 
 
 @app.callback(
-    Output("select-variable", "options"),
-    [Input("dataframe-values", "children")],
-)
-def render_navigation_variables(payload):
-    """
-    Compute list of items from "values" data for populating the "variables"
-    chooser element.
-    """
-    climate_data = pd.read_json(payload, orient="split")
-    log.info(f"Rendering variable dropdown from {frame_summary(climate_data)}")
-
-    # Build list of columns to be selectable.
-    columns = []
-    for column in climate_data.columns:
-
-        # Skip some columns.
-        if column in ["station_id", "date"]:
-            continue
-
-        columns.append({"label": column, "value": column})
-
-    return columns
-
-
-@app.callback(
     Output("status-response-stations", "children"),
     [
-        Input("select-parameter", "value"),
+        Input("select-provider", "value"),
+        Input("select-network", "value"),
         Input("select-resolution", "value"),
+        Input("select-dataset", "value"),
+        Input("select-parameter", "value"),
         Input("select-period", "value"),
         Input("dataframe-stations", "children"),
     ],
 )
-def render_status_response_stations(parameter: str, resolution: str, period: str, payload: str):
+def render_status_response_stations(
+    provider: str, network: str, resolution: str, dataset: str, parameter: str, period: str, payload: str
+):
     """
     Report about the status of the query.
     """
 
     title = [dcc.Markdown("#### Stations")]
 
+    if not (provider and network and resolution and dataset and parameter and period):
+        empty_message = [dcc.Markdown("Choose from provider, network, resolution, dataset,  parameter and period.")]
+
+        return title + empty_message
+
     empty_message = [
         dcc.Markdown(
             f"""
-No data.
-Maybe the combination of "{parameter}", "{resolution}" and "{period}" is invalid.
-    """
+            No data. Maybe the combination of "{resolution}", "{dataset}", "{parameter}" and "{period}"
+            is invalid for provider "{provider}" and network "{network}".
+            """
         )
     ]
 
@@ -240,20 +263,24 @@ Maybe the combination of "{parameter}", "{resolution}" and "{period}" is invalid
     Output("status-response-values", "children"),
     [
         Input("dataframe-values", "children"),
-        State("select-parameter", "value"),
+        State("select-provider", "value"),
+        State("select-network", "value"),
         State("select-resolution", "value"),
+        State("select-dataset", "value"),
+        State("select-parameter", "value"),
         State("select-period", "value"),
         State("select-station", "value"),
-        State("select-variable", "value"),
     ],
 )
 def render_status_response_values(
     payload: str,
-    parameter: str,
+    provider: str,
+    network: str,
     resolution: str,
+    dataset: str,
+    parameter: str,
     period: str,
     station: str,
-    variable: str,
 ):
     """
     Report about the status of the query.
@@ -267,7 +294,7 @@ def render_status_response_values(
         # Main message.
         empty_message = [html.Span("No data. ")]
 
-        candidates = ["parameter", "resolution", "period", "station", "variable"]
+        candidates = ["provider", "network", "resolution", "dataset", "parameter", "period", "station"]
         missing = []
         for candidate in candidates:
             if locals().get(candidate) is None:
@@ -353,10 +380,9 @@ def render_map(payload):
 
 @app.callback(
     Output("graph-values", "figure"),
-    [Input("select-variable", "value")],
-    [Input("dataframe-values", "children")],
+    [Input("select-parameter", "value"), Input("dataframe-values", "children")],
 )
-def render_graph(variable, payload):
+def render_graph(parameter, payload):
     """
     Create a "graph" Figure element from "values" data.
     """
@@ -366,9 +392,9 @@ def render_graph(variable, payload):
     except ValueError:
         climate_data = pd.DataFrame()
 
-    log.info(f"Rendering graph for variable={variable} from {frame_summary(climate_data)}")
+    log.info(f"Rendering graph for parameter={parameter} from {frame_summary(climate_data)}")
 
-    fig = default_figure(climate_data, variable)
+    fig = default_figure(climate_data, parameter)
 
     fig.update_layout(
         margin=go.layout.Margin(
@@ -380,6 +406,140 @@ def render_graph(variable, payload):
     )
 
     return fig
+
+
+@app.callback(Output("select-network", "options"), Input("select-provider", "value"))
+def set_network_options(provider):
+    """Set network options based on provider"""
+    if not provider:
+        return []
+
+    return [{"label": network.name, "value": network.name} for network in ApiEndpoints[provider]]
+
+
+@app.callback(
+    Output("select-resolution", "options"),
+    [
+        Input("select-provider", "value"),
+        Input("select-network", "value"),
+    ],
+)
+def set_resolution_options(provider, network):
+    """Set resolution options based on provider and network"""
+    if not (provider and network):
+        return []
+
+    api = ApiEndpoints[provider][network].value
+
+    return [{"label": resolution.name, "value": resolution.name} for resolution in api._resolution_base]
+
+
+@app.callback(
+    Output("select-dataset", "options"),
+    [
+        Input("select-provider", "value"),
+        Input("select-network", "value"),
+        Input("select-resolution", "value"),
+    ],
+)
+def set_dataset_options(provider, network, resolution):
+    """Set dataset options based on provider network and resolution"""
+    if not (provider and network and resolution):
+        return []
+
+    api = ApiEndpoints[provider][network].value
+
+    if api._has_datasets and not api._unique_dataset:
+        # first dataset is placeholder for unique dataset with parameters combined from all datasets
+        datasets = [{"label": resolution, "value": resolution}]
+        for dataset in api._dataset_tree[resolution]:
+            datasets.append({"label": dataset, "value": dataset})
+        return datasets
+    else:
+        return [{"label": resolution, "value": resolution}]
+
+
+@app.callback(
+    [
+        Output("select-parameter", "options"),
+        Output("select-period", "options"),
+    ],
+    [
+        Input("select-provider", "value"),
+        Input("select-network", "value"),
+        Input("select-resolution", "value"),
+        Input("select-dataset", "value"),
+    ],
+)
+def set_parameter_options(provider, network, resolution, dataset):
+    """Set parameter options based on provider, network, resolution and dataset"""
+    if not (provider and network and resolution and dataset):
+        return [], []
+
+    api = ApiEndpoints[provider][network].value
+
+    if api._has_datasets and not api._unique_dataset:
+        if dataset == resolution:
+            # Return tidy parameters e.g. parameters grouped under one resolution
+            parameters = [
+                {"label": parameter.name, "value": parameter.name} for parameter in api._parameter_base[resolution]
+            ]
+        else:
+            # return individual parameters of one dataset e.g. climate_summary
+            parameters = [
+                {"label": parameter.name, "value": parameter.name}
+                for parameter in api._dataset_tree[resolution][dataset]
+            ]
+    else:
+        # If network only provides parameters but not datasets, just return them all as option
+        parameters = [{"label": parameter.name, "value": parameter.name} for parameter in api._dataset_tree[resolution]]
+
+    # Periods ALL placeholder, may use click options for multiple periods
+    periods = [{"label": "ALL", "value": "ALL"}]
+    for period in api._period_base:
+        periods.append({"label": period.name, "value": period.name})
+
+    return parameters, periods
+
+
+@app.callback(
+    [
+        Output("select-network", "value"),
+        Output("select-resolution", "value"),
+        Output("select-dataset", "value"),
+        Output("select-parameter", "value"),
+        Output("select-period", "value"),
+    ],
+    [
+        Input("select-provider", "value"),
+        Input("select-network", "value"),
+        Input("select-resolution", "value"),
+        Input("select-dataset", "value"),
+        Input("select-parameter", "value"),
+        Input("select-period", "value"),
+    ],
+)
+def reset_values(provider, network, resolution, dataset, parameter, period):
+    """Reset settings values if any previous parameter has been changed e.g.
+    when a new provider is selected, reset network, resolution, etc"""
+    last_triggered = dash.callback_context.triggered[0]["prop_id"].split(".")[0]
+
+    previous = ("select-provider", "select-network", "select-resolution", "select-dataset")
+
+    if last_triggered in previous[:1]:
+        network = None
+
+    if last_triggered in previous[:2]:
+        resolution = None
+
+    if last_triggered in previous[:3]:
+        dataset = None
+
+    if last_triggered in previous:
+        parameter = None
+        period = None
+
+    return network, resolution, dataset, parameter, period
 
 
 def start_service(listen_address: Optional[str] = None, reload: Optional[bool] = False):  # pragma: no cover
@@ -394,7 +554,8 @@ def start_service(listen_address: Optional[str] = None, reload: Optional[bool] =
 
     host, port = listen_address.split(":")
     port = int(port)
-    app.server.run(host=host, port=port, debug=reload)
+
+    app.run_server(host=host, port=port, debug=reload)
 
 
 if __name__ == "__main__":
