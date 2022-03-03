76702b1d248a49bdb45f5b7915789a40b2623bcb
Andreas Motl <andreas.motl@panodata.org>
2022-03-03 11:04:50 +0100

    Compare `lxml-stream` against `pykml`

--- wetterdienst/provider/dwd/mosmix/mosmix_large_stream.py
+++ wetterdienst/provider/dwd/mosmix/mosmix_large_stream.py
@@ -9,19 +9,33 @@ Reference
 
 - http://blog.behnel.de/posts/faster-xml-stream-processing-in-python.html
 
+
+Setup
+=====
+::
+
+    pip install lxml pykml psutil
+
+
 Synopsis
 ========
 ::
 
+    # Acquire data.
     wget https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/all_stations/kml/MOSMIX_L_LATEST.kmz
     unzip MOSMIX_L_LATEST.kmz
-    time python wetterdienst/provider/dwd/mosmix/mosmix_large_stream.py MOSMIX_L_2022030215.kml
+
+    # Decode data.
+    KMLFILE=MOSMIX_L_2022030215.kml
+    time python wetterdienst/provider/dwd/mosmix/mosmix_large_stream.py lxml-stream ${KMLFILE}
+    time python wetterdienst/provider/dwd/mosmix/mosmix_large_stream.py pykml ${KMLFILE}
 
 """
 import sys
-import lxml.etree as ET
 from pathlib import Path
 from typing import Union
+
+import lxml.etree as ET
 
 MOSMIX_L_URL = "https://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/all_stations/kml/MOSMIX_L_LATEST.kmz"
 
@@ -31,29 +45,55 @@ class KMLStreamReader:
         self.uri = uri
 
     def read(self):
-        print(f"Parsing KML")
         count = 0
-        for event, elem in ET.iterparse(self.uri, events=("end",)):
+        for event, element in ET.iterparse(self.uri, events=("end",)):
             if event == "end":
                 # if elem.tag == 'location' and elem.text and 'Africa' in elem.text:
                 #    count += 1
                 #
-                #print(f"Element: {elem}")
-                elem.clear()
+                # print(f"Element: {element}")
+                element.clear()
                 count += 1
         return count
 
 
 def memory_used():
-    # pip intall psutil
-    import os, psutil
+    # pip install psutil
+    import os
+
+    import psutil
+
     process = psutil.Process(os.getpid())
     return process.memory_info().rss
 
 
-if __name__ == "__main__":
-    path = sys.argv[1]
+def parse_lxml_stream(path: str):
     ksr = KMLStreamReader(uri=path)
     count = ksr.read()
     print(f"Elements seen: {count}")
     print(f"Memory used:   {memory_used()}")
+
+
+def parse_pykml(path: str):
+    from pykml.parser import parse
+
+    with open(path, "rb") as f:
+        obj = parse(f)
+        count = 0
+        for element in obj.iter():
+            # print(f"Element: {element}")
+            # element.clear()
+            count += 1
+        print(f"Elements seen: {count}")
+        print(f"Memory used:   {memory_used()}")
+
+
+if __name__ == "__main__":
+    parser = sys.argv[1]
+    path = sys.argv[2]
+    if parser == "lxml-stream":
+        parse_lxml_stream(path)
+    elif parser == "pykml":
+        parse_pykml(path)
+    else:
+        raise KeyError(f"Unknown parser")
