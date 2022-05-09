88ac1369724d08ba87e234a02846ec4172823f84
Benjamin Gutzmann <gutzemann@gmail.com>
2022-05-09 17:35:24 +0200

    Use ssl verification again

--- wetterdienst/util/pdf.py
+++ wetterdienst/util/pdf.py
@@ -8,9 +8,6 @@ import PyPDF2
 from wetterdienst.util.cache import CacheExpiry
 from wetterdienst.util.network import download_file
 
-from wetterdienst.util.cache import CacheExpiry
-from wetterdienst.util.network import download_file
-
 
 def read_pdf(url):
     text = StringIO()
