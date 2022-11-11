142d327cd7ebce9151aee4b973d46ba19466e309
Benjamin Gutzmann <gutzemann@gmail.com>
2022-11-12 00:36:20 +0100

    Fix test for DWD observation empty data

--- tests/provider/dwd/observation/test_api_data.py
+++ tests/provider/dwd/observation/test_api_data.py
@@ -394,13 +394,13 @@ def test_dwd_observation_data_result_all_missing_data():
     stations = DwdObservationRequest(
         parameter=Parameter.PRECIPITATION_HEIGHT.name,
         resolution=DwdObservationResolution.MINUTE_10,
-        start_date=datetime(2021, 10, 1),
+        start_date=datetime(2021, 10, 3),
         end_date=datetime(2021, 10, 5),
-    ).filter_by_station_id(["01851"])
+    ).filter_by_station_id(["05435"])
 
     values = stations.values.all().df
 
-    assert all(values.value.isna())
+    assert values.value.dropna().empty
 
 
 @pytest.mark.remote
