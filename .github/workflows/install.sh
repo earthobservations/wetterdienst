cf947ec06ec5b5d23d47a736664d4f7fc670955f
Andreas Motl <andreas.motl@panodata.org>
2022-11-16 02:59:31 +0100

    CI: Skip installing `h5py` on Python 3.11

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -13,8 +13,14 @@ set -e
 set -x
 
 if [ "${flavor}" = "testing" ]; then
-  poetry install --verbose --no-interaction --extras=sql --extras=export --extras=restapi --extras=explorer --extras=interpolation --extras=radar
+
+  poetry install --verbose --no-interaction --extras=sql --extras=export --extras=restapi --extras=explorer --extras=interpolation
   poetry run pip install --verbose --no-input --no-deps wradlib
+
+  # Wheels for `h5py` not available for cp311 yet.
+  poetry run python -c 'import sys; sys.exit(not sys.version_info < (3, 11))' \
+    && poetry run pip install --verbose --no-input --no-deps h5py \
+    || true
 
 elif [ "${flavor}" = "docs" ]; then
   poetry install --verbose --no-interaction --extras=docs --extras=interpolation
