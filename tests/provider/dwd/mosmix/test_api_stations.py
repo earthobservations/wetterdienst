d1ae474a5fa4717b428cc25e20cd2c07f328cc95
Andreas Motl <andreas.motl@panodata.org>
2023-01-19 15:20:43 +0100

    DWD MOSMIX: Improve test cases

--- tests/provider/dwd/mosmix/test_api_stations.py
+++ tests/provider/dwd/mosmix/test_api_stations.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018-2021, earthobservations developers.
 # Distributed under the MIT License. See LICENSE for more info.
+"""
+Verify acquisition of DWD MOSMIX station list.
+
+Please note that in the original data, the values in the `LAT` and `LON`
+columns are in "Degrees Minutes" format (`<degrees>.<degrees minutes>`.),
+and not in "Decimal Degrees".
+
+Source: https://www.dwd.de/EN/ourservices/met_application_mosmix/mosmix_stations.cfg?view=nasPublication&nn=495490
+"""
+from unittest import mock
+
 import pandas as pd
 import pytest
-from pandas._testing import assert_frame_equal
+from pandas._testing import assert_frame_equal, assert_series_equal
 
 from wetterdienst.metadata.columns import Columns
 from wetterdienst.provider.dwd.mosmix import DwdMosmixRequest, DwdMosmixType
@@ -11,19 +22,76 @@ from wetterdienst.provider.dwd.mosmix import DwdMosmixRequest, DwdMosmixType
 
 @pytest.mark.remote
 def test_dwd_mosmix_stations_success():
-    # Existing combination of parameters
-    request = DwdMosmixRequest(parameter="small", mosmix_type=DwdMosmixType.LARGE)
+    """
+    Verify full MOSMIX station list.
+    """
 
-    df = request.all().df
-
+    # Acquire data.
+    df = DwdMosmixRequest(parameter="small", mosmix_type=DwdMosmixType.LARGE).all().df
     assert not df.empty
 
+    # Verify size of dataframe with all records.
+    assert df.shape == (mock.ANY, 9)
+    assert df.shape[0] >= 5500
+
+    # Verify content of dataframe. For reference purposes,
+    # we use the first and the last record of the list.
+    first_station_reference = pd.Series(
+        {
+            "station_id": "01001",
+            "icao_id": "ENJA",
+            "from_date": pd.NaT,
+            "to_date": pd.NaT,
+            "height": 10.0,
+            "latitude": 70.93,
+            "longitude": -8.0,
+            "name": "JAN MAYEN",
+            "state": pd.NA,
+        },
+    )
+    last_station_reference = pd.Series(
+        {
+            "station_id": "Z949",
+            "icao_id": pd.NA,
+            "from_date": pd.NaT,
+            "to_date": pd.NaT,
+            "height": 1200.0,
+            "latitude": 47.58,
+            "longitude": 13.02,
+            "name": "JENNER",
+            "state": pd.NA,
+        },
+    )
+
+    first_station: pd.Series = df.head(1).iloc[0]
+    last_station: pd.Series = df.tail(1).iloc[0]
+
+    first_station.name = None
+    last_station.name = None
+
+    assert_series_equal(first_station, first_station_reference)
+    assert_series_equal(last_station, last_station_reference)
+
+
+@pytest.mark.remote
+def test_dwd_mosmix_stations_filtered():
+    """
+    Verify MOSMIX station list filtering by station identifier.
+    """
+
+    # Acquire data.
+    request = DwdMosmixRequest(parameter="small", mosmix_type=DwdMosmixType.LARGE)
+    df = request.all().df
+    assert not df.empty
+
+    # Filter dataframe.
     df_given = (
         df.loc[df[Columns.STATION_ID.value].isin(["01001", "72306", "83891", "94767"]), :]
         .sort_values("station_id")
         .reset_index(drop=True)
     )
 
+    # Verify content of filtered dataframe.
     df_expected = pd.DataFrame(
         [
             {
