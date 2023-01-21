e99793a8f1bbc9a1d40b810f485b580ca06eb5f4
Andreas Motl <andreas.motl@panodata.org>
2023-01-21 11:10:47 +0100

    Chore: `numpy.distutils` raises a deprecation notice

--- wetterdienst/util/python.py
+++ wetterdienst/util/python.py
@@ -1,3 +1,9 @@
+import collections.abc as collections_abc
+
+string_types = (str,)
+binary_types = (bytes,)
+
+
 class classproperty(property):
     """
     A decorator that behaves like @property except that operates
@@ -12,3 +18,19 @@ class classproperty(property):
 
     def __get__(desc, self, cls):
         return desc.fget(cls)
+
+
+def to_list(x, default=None):
+    """
+    Conveniently wrap single items into list, while keeping lists as is.
+
+    From `sqlalchemy.util._collection`.
+    """
+    if x is None:
+        return default
+    if not isinstance(x, collections_abc.Iterable) or isinstance(x, string_types + binary_types):
+        return [x]
+    elif isinstance(x, list):
+        return x
+    else:
+        return list(x)
