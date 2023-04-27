27ffdc3b6cedbce42cb060a43404a9949a31084a
Benjamin Gutzmann <gutzemann@gmail.com>
2023-04-27 20:08:28 +0200

    Update benchmarks

--- benchmarks/interpolation_over_time.py
+++ benchmarks/interpolation_over_time.py
@@ -37,16 +37,29 @@ def get_regular_df(parameter: str, start_date: datetime, end_date: datetime, exc
     return df[df["station_id"] == first_station_id]
 
 
-def get_rmse(regular_values, interpolated_values):
-    n = regular_values.size
-    return (((regular_values - interpolated_values).dropna() ** 2).sum() / n) ** 0.5
+def get_rmse(regular_values: pd.Series, interpolated_values: pd.Series):
+    diff = (regular_values.reset_index(drop=True) - interpolated_values.reset_index(drop=True)).dropna()
+    n = diff.size
+    return ((diff**2).sum() / n) ** 0.5
 
 
-def visualize(regular_df: pd.DataFrame, interpolated_df: pd.DataFrame):
-    rmse = get_rmse(regular_df.value.reset_index(drop=True), interpolated_df.value.reset_index(drop=True))
-    plt.plot_date(regular_df["date"], regular_df["value"], fmt="red", label="regular")
-    plt.plot_date(interpolated_df["date"], interpolated_df["value"], fmt="black", label="interpolated")
-    title = f"RMSE: {np.round(rmse, 2)}"
+def get_corr(regular_values: pd.Series, interpolated_values: pd.Series):
+    return np.corrcoef(regular_values.to_list(), interpolated_values.to_list())[0][1].item()
+
+
+def visualize(parameter: str, unit: str, regular_df: pd.DataFrame, interpolated_df: pd.DataFrame):
+    rmse = get_rmse(regular_df["value"], interpolated_df["value"])
+    corr = get_corr(regular_df["value"], interpolated_df["value"])
+    factor = 0.5
+    plt.figure(figsize=(factor * 19.2, factor * 10.8))
+    plt.plot(regular_df["date"], regular_df["value"], color="red", label="regular")
+    plt.plot(interpolated_df["date"], interpolated_df["value"], color="black", label="interpolated")
+    ylabel = f"{parameter.lower()} [{unit}]"
+    plt.ylabel(ylabel)
+    title = (
+        f"rmse: {np.round(rmse, 2)}, corr: {np.round(corr, 2)}\n"
+        f"station_ids: {interpolated_df['station_ids'].to_list()[0]}"
+    )
     plt.title(title)
     plt.legend()
     plt.tight_layout()
@@ -55,12 +68,13 @@ def visualize(regular_df: pd.DataFrame, interpolated_df: pd.DataFrame):
 
 def main():
     parameter = Parameter.TEMPERATURE_AIR_MEAN_200.name
+    unit = "K"
     start_date = datetime(2022, 1, 1)
     end_date = datetime(2022, 2, 24)
     interpolated_df = get_interpolated_df(parameter, start_date, end_date)
     exclude_stations = interpolated_df.station_ids[0]
     regular_df = get_regular_df(parameter, start_date, end_date, exclude_stations)
-    visualize(regular_df, interpolated_df)
+    visualize(parameter, unit, regular_df, interpolated_df)
 
 
 if __name__ == "__main__":
