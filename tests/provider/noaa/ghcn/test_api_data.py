184ba13315714100f0a398cd89416bb349434ac0
Benjamin Gutzmann <gutzemann@gmail.com>
2023-05-07 20:39:25 +0200

    Tests: Fix NOAA GHCN Amsterdam test

--- tests/provider/noaa/ghcn/test_api_data.py
+++ tests/provider/noaa/ghcn/test_api_data.py
@@ -25,18 +25,19 @@ def test_api_amsterdam(start_date, end_date, default_settings):
         settings=default_settings,
     ).filter_by_name("DE BILT")
     given_df = request.values.all().df
+    given_df_filtered = given_df[given_df["date"] == pd.Timestamp("2021-01-01 23:00:00+00:00")].reset_index(drop=True)
     expected_df = pd.DataFrame(
         {
             "station_id": pd.Categorical(["NLM00006260"]),
             "dataset": pd.Categorical(["daily"]),
             "parameter": pd.Categorical(["temperature_air_mean_200"]),
-            "date": [pd.Timestamp("2015-04-15 22:00:00+0000", tz="UTC")],
-            "value": [282.75],
+            "date": [pd.Timestamp("2021-01-01 23:00:00+0000", tz="UTC")],
+            "value": [276.84999999999997],
             "quality": [np.nan],
         }
     )
     assert_frame_equal(
-        given_df[given_df["date"] == pd.Timestamp("2015-04-15 22:00:00+00:00")].reset_index(drop=True),
+        given_df_filtered,
         expected_df,
         check_categorical=False,
     )
