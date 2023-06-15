b05af1627d41660bfa7eba50a0eee6ff96770c9b
Benjamin Gutzmann <gutzemann@gmail.com>
2023-06-15 08:20:51 +0200

    Revert "Update RoadWeather Project"
    This reverts commit 1fc3359f18c56930ee2e15bb88475f5d8d08bc72.

--- wetterdienst/metadata/columns.py
+++ wetterdienst/metadata/columns.py
@@ -43,10 +43,3 @@ class Columns(Enum):
     # Columns used for interpolation
     DISTANCE_MEAN = "distance_mean"
     STATION_IDS = "station_ids"
-    # for road weather
-    ROAD_NAME = "road_name"
-    ROAD_TYPE = "road_type"
-    ROAD_SURFACE_TYPE = "road_type"
-    STATION_GROUP = "station_group"
-    ROAD_SECTOR = "road_Sector"
-    ROAD_SURROUNDINGS_TYPE = "road_surroundings_type"
