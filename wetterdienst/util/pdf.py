3bd120ed91c5dc8cb226e1a51ed7c8eb8afa5ed2
Benjamin Gutzmann <gutzemann@gmail.com>
2023-07-11 23:57:44 +0200

    Replace PyPDF2 by pypdf

--- wetterdienst/util/pdf.py
+++ wetterdienst/util/pdf.py
@@ -3,7 +3,7 @@
 # Distributed under the MIT License. See LICENSE for more info.
 from io import StringIO
 
-import PyPDF2
+import pypdf
 
 from wetterdienst.settings import Settings
 from wetterdienst.util.cache import CacheExpiry
@@ -13,7 +13,7 @@ from wetterdienst.util.network import download_file
 def read_pdf(url):
     text = StringIO()
     response = download_file(url, settings=Settings.default(), ttl=CacheExpiry.NO_CACHE)
-    pdf = PyPDF2.PdfReader(response)
+    pdf = pypdf.PdfReader(response)
     for page_number in range(len(pdf.pages)):
         page = pdf.pages[page_number]
         result = page.extract_text()
