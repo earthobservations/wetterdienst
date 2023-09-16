d94d2a2efe1f4bba8c902a174d3e33222a68f10b
Benjamin Gutzmann <gutzemann@gmail.com>
2023-09-16 17:03:24 +0200

    Restructure Mosmix module

--- wetterdienst/provider/dwd/mosmix/__init__.py
+++ wetterdienst/provider/dwd/mosmix/__init__.py
@@ -2,9 +2,8 @@
 # Copyright (C) 2018-2021, earthobservations developers.
 # Distributed under the MIT License. See LICENSE for more info.
 from wetterdienst.provider.dwd.mosmix.api import (
+    DwdForecastDate,
     DwdMosmixRequest,
     DwdMosmixStationGroup,
-    DwdMosmixValues,
+    DwdMosmixType,
 )
-from wetterdienst.provider.dwd.mosmix.metadata.dates import DwdForecastDate
-from wetterdienst.provider.dwd.mosmix.metadata.mosmix_type import DwdMosmixType
--- wetterdienst/provider/dwd/mosmix/metadata/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2018-2021, earthobservations developers.
-# Distributed under the MIT License. See LICENSE for more info.
-from wetterdienst.provider.dwd.mosmix.metadata.dates import DwdForecastDate
-from wetterdienst.provider.dwd.mosmix.metadata.mosmix_type import DwdMosmixType
-from wetterdienst.provider.dwd.mosmix.metadata.parameter import DwdMosmixParameter
