110ad1a89975748d9f37c446f7641658c3f11ab2
Benjamin Gutzmann <gutzemann@gmail.com>
2023-09-16 22:24:44 +0200

    Add DwdForecastDate import in DWD DMO module

--- wetterdienst/provider/dwd/dmo/__init__.py
+++ wetterdienst/provider/dwd/dmo/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018-2023, earthobservations developers.
 # Distributed under the MIT License. See LICENSE for more info.
-from wetterdienst.provider.dwd.dmo.api import DwdDmoRequest, DwdDmoStationGroup, DwdDmoType
+from wetterdienst.provider.dwd.dmo.api import DwdDmoRequest, DwdDmoStationGroup, DwdDmoType, DwdForecastDate
