871fb0d047f6f8edf0c250ba95530396f59fd3a2
Benjamin Gutzmann <gutzemann@gmail.com>
2023-09-16 17:03:24 +0200

    Restructure Mosmix module

--- wetterdienst/provider/dwd/mosmix/metadata/mosmix_type.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2018-2021, earthobservations developers.
-# Distributed under the MIT License. See LICENSE for more info.
-from enum import Enum
-
-
-class DwdMosmixType(Enum):
-    SMALL = "SMALL"
-    LARGE = "LARGE"
