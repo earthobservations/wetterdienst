e5230dace17242bd4ce92fb7eb2a25e4725295c0
Benjamin Gutzmann <gutzemann@gmail.com>
2023-09-28 00:33:34 +0200

    Add sql query function to streamlit app

--- wetterdienst/ui/streamlit/app.py
+++ wetterdienst/ui/streamlit/app.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018-2023, earthobservations developers.
 # Distributed under the MIT License. See LICENSE for more info.
+import duckdb
 import plotly.express as px
 import polars as pl
 import streamlit
 import streamlit as st
 
 from wetterdienst.provider.dwd.observation import DwdObservationRequest
+
+SQL_DEFAULT = """
+SELECT * 
+FROM df
+WHERE value IS NOT NULL
+""".strip()
 
 request = DwdObservationRequest("climate_summary", "daily")
 
@@ -28,14 +35,16 @@ def get_dwd_observation_station_values(station_id):
 
 def main():
     """Small streamlit app for accessing German climate stations by DWD"""
-    st.title("Wetterdienst - Data Tool")
+    title = "Wetterdienst - Data Tool"
+    st.set_page_config(page_title=title)
+    st.title(title)
 
     st.subheader("Introduction")
     st.markdown(
         """
         This is a streamlit app based on the [wetterdienst](https://github.com/earthobservations/wetterdienst) 
         library that allows analysis of German climate stations (internally phrased "climate summary") by
-        the [German Weather Service (DWD)](https://www.dwd.de/). There are over 1_500 climate stations in Germany and
+        the [Deutscher Wetterdienst](https://www.dwd.de/). There are over 1_500 climate stations in Germany and
         all of the data can be accessed freely thanks to the open data initiative. The app enables you to select any 
         of the stations (by station id or name), download its data (as CSV) and get visualizations of it. 
         """
@@ -53,9 +62,23 @@ def main():
         st.map(get_dwd_observation_station(station["station_id"]).df)
 
     st.subheader("DataFrame")
+    st.info(
+        """
+        Use [duckdb](https://duckdb.org/docs/sql/introduction.html) sql queries to transform the data. 
+        Important: 
+          - use **FROM df** 
+          - make sure the result has **parameter**, **date** and **value** columns.
+        """
+    )
+    sql_query = st.text_area(
+        "sql query",
+        value=SQL_DEFAULT,
+    )
     df = pl.DataFrame()
     if station:
         df = get_dwd_observation_station_values(station["station_id"]).df
+        if sql_query:
+            df = duckdb.query(sql_query).pl()
         st.dataframe(df, hide_index=True, use_container_width=True)
         st.download_button("Download CSV", df.write_csv(), "data.csv", "text/csv")
 
@@ -81,6 +104,18 @@ def main():
                 fig.layout[axis_name].title.text = annotation.text
             annotation.text = ""
         st.plotly_chart(fig)
+
+    st.subheader("Credits")
+    st.markdown(
+        """
+        This app is powered by [wetterdienst](https://github.com/earthobservations/wetterdienst) from 
+        [earthobservations](https://github.com/earthobservations). Credits for the data go to 
+        [Deutscher Wetterdienst](https://www.dwd.de) - Germany's national meteorological service - for
+        publishing their data as **open data**. Credits also go to [streamlit](https://streamlit.io/) for hosting this 
+        app. If you have any issues or ideas regarding this app, please let us know in the 
+        [issues](https://github.com/earthobservations/wetterdienst/issues).
+        """
+    )
 
 
 if __name__ == "__main__":
