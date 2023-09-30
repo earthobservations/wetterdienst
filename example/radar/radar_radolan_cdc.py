0ed6621265bffbf87311a4ff09b38f07d6a2d0ec
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2023-09-30 10:52:38 +0200

    Update radar_radolan_cdc.py

--- example/radar/radar_radolan_cdc.py
+++ example/radar/radar_radolan_cdc.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2018-2021, earthobservations developers.
+# Copyright (C) 2018-2023, earthobservations developers.
 # Distributed under the MIT License. See LICENSE for more info.
 """
 =====
@@ -33,9 +33,9 @@ real-time for Germany.
 - https://www.dwd.de/EN/Home/_functions/aktuelles/2019/20190820_radolan.html
 - https://www.dwd.de/DE/leistungen/radolan/radolan_info/radolan_poster_201711_en_pdf.pdf?__blob=publicationFile&v=2
 - https://opendata.dwd.de/climate_environment/CDC/grids_germany/daily/radolan/
-- https://docs.wradlib.org/en/stable/notebooks/radolan/radolan_showcase.html#RADOLAN-Composite
-- Hourly: https://docs.wradlib.org/en/stable/notebooks/radolan/radolan_showcase.html#RADOLAN-RW-Product
-- Daily: https://docs.wradlib.org/en/stable/notebooks/radolan/radolan_showcase.html#RADOLAN-SF-Product
+- https://docs.wradlib.org/en/stable/notebooks/fileio/radolan/radolan_showcase.html#RADOLAN-Composite
+- Hourly: https://docs.wradlib.org/en/stable/notebooks/fileio/radolan/radolan_showcase.html#RADOLAN-RW-Product
+- Daily: https://docs.wradlib.org/en/stable/notebooks/fileio/radolan/radolan_showcase.html#RADOLAN-SF-Product
 """  # noqa:D205,D400,E501
 import logging
 import os
@@ -56,48 +56,20 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 
 
-def plot(data: np.ndarray, attributes: dict, label: str):
-    """Plot RADOLAN data with prefixed settings."""
-    # Get coordinates.
-    radolan_grid_xy = wrl.georef.get_radolan_grid(900, 900)
-
-    # Mask data.
-    data = np.ma.masked_equal(data, -9999)
-
-    # Plot with matplotlib.
-    plot_radolan(data, attributes, radolan_grid_xy, clabel=label)
-
-
-def plot_radolan(data: np.ndarray, attrs: dict, grid: np.dstack, clabel: str = None):
+def plot_radolan(ds: xr.Dataset, clabel: str = None):
     """Plot RADOLAN data.
 
     Shamelessly stolen from the wradlib RADOLAN Product Showcase documentation.
-    https://docs.wradlib.org/en/stable/notebooks/radolan/radolan_showcase.html
+    https://docs.wradlib.org/en/stable/notebooks/fileio/radolan/radolan_showcase.html
 
     Thanks!
+    It's a pleasure!
     """
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, aspect="equal")
-    x = grid[:, :, 0]
-    y = grid[:, :, 1]
-    pm = ax.pcolormesh(x, y, data, cmap="viridis", shading="auto")
-    cb = fig.colorbar(pm, shrink=0.75)
-    cb.set_label(clabel)
-    plt.xlabel("x [km]")
-    plt.ylabel("y [km]")
-    plt.title("{0} Product\n{1}".format(attrs["producttype"], attrs["datetime"].isoformat()))
-    plt.xlim((x[0, 0], x[-1, -1]))
-    plt.ylim((y[0, 0], y[-1, -1]))
+    pm = ds[label].plot(ax=ax, cmap="viridis", shading="auto")
+    plt.title(f"{label} Product\n{ds.time.min()}")
     plt.grid(color="r")
-
-
-def radolan_info(data: np.ndarray, attributes: dict):
-    """Display metadata from RADOLAN request."""
-    log.info("Data shape: %s", data.shape)
-    log.info("Attributes:")
-
-    for key, value in attributes.items():
-        print(f"- {key}: {value}")
 
 
 def label_by_producttype(producttype: str) -> Optional[str]:
@@ -128,13 +100,16 @@ def radolan_grid_example():
     for item in radolan.query():
         # Decode data using wradlib.
         log.info("Parsing RADOLAN_CDC composite data for %s", item.timestamp)
-        data, attributes = wrl.io.read_radolan_composite(item.data)
+        ds = xr.open_dataset(item.data, engine="radolan")
 
         # Compute label matching RW/SF product.
-        label = label_by_producttype(attributes["producttype"])
+        label = label_by_producttype(list(ds.data_vars.keys())[0])
+
+        # print Dataset    
+        print(ds)
 
         # Plot and display data.
-        plot(data, attributes, label)
+        plot(ds, label)
         if "PYTEST_CURRENT_TEST" not in os.environ:
             plt.show()
 
