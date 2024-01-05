2336eed33acaec6636a2e9feb014ead6538d3af6
Benjamin Gutzmann <gutzemann@gmail.com>
2024-01-05 16:54:18 +0100

    Add test for dwd climate summary xarray example

--- examples/dwd_climate_summary_xarray_dump.py
+++ examples/dwd_climate_summary_xarray_dump.py
@@ -1,43 +1,43 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018-2021, earthobservations developers.
 # Distributed under the MIT License. See LICENSE for more info.
+import os
+from pathlib import Path
+
 import xarray as xr
 import zarr
 from tqdm import tqdm
 
 from wetterdienst.provider.dwd.observation import DwdObservationRequest
 
+ROOT = Path(__file__).parent.parent
 
-def create_dwd_kl_zarr_dump(path):
+
+def create_dwd_climate_summary_zarr_dump(path):
     request = DwdObservationRequest(
         "kl",
         "daily",
         "historical",
     ).all()
-
     meta = request.df
     store = zarr.DirectoryStore(path)
-
     data = []
-
     for result in tqdm(request.values.query(), total=meta.shape[0]):
-        df = result.df
-        df[["station_id", "dataset", "parameter"]] = df[["station_id", "dataset", "parameter"]].astype(str)
+        df = result.df.drop(["quality"]).to_pandas()
         df.date = df.date.map(lambda date: date.to_datetime64())
-        df = df.set_index(["station_id", "dataset", "parameter", "date"], inplace=False).drop(
-            columns=["quality"]
-        )  # "latitude", "longitude",
+        df = df.set_index(["station_id", "dataset", "parameter", "date"])
         ds = df.to_xarray()
         data.append(ds)
-        data.append(df.to_xarray())
-
+        if "PYTEST_CURRENT_TEST" in os.environ:
+            break
     ds = xr.concat(data, dim="station_id")
-    ds.to_zarr(store, mode="w")
+    if "PYTEST_CURRENT_TEST" not in os.environ:
+        ds.to_zarr(store, mode="w")
 
 
 def main():
     # this takes something like 15 min and will require roughly 1 gb on disk
-    create_dwd_kl_zarr_dump("dwd_kl.zarr")
+    create_dwd_climate_summary_zarr_dump(ROOT / "dwd_climate_summary.zarr")
 
 
 if __name__ == "__main__":
