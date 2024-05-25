bd92c0f2592a38b831e9f5268c814691c1deb18e
Benjamin Gutzmann <gutzemann@gmail.com>
2024-05-26 00:07:10 +0200

    Add example for duckdb dump

---
+++ examples/provider/dwd/observation/dwd_obs_climate_summary_duckdb_dump.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2018-2021, earthobservations developers.
+# Distributed under the MIT License. See LICENSE for more info.
+import os
+from pathlib import Path
+
+import duckdb
+from tqdm import tqdm
+
+from wetterdienst.provider.dwd.observation import DwdObservationRequest
+
+ROOT = Path(__file__).parent.parent
+
+
+def create_dwd_climate_summary_duckdb_dump(path, test):
+    request = DwdObservationRequest(
+        "kl",
+        "daily",
+        "historical",
+    ).filter_by_rank(latlon=(47.5, 7.5), rank=10)
+    connection = f"duckdb:////{path}"
+    request.to_target(f"{connection}?table=stations")
+    for result in tqdm(request.values.query(), total=request.df.shape[0]):
+        result.to_target(f"{connection}?table=values")
+        if test:
+            break
+
+
+def main(filepath, test):
+    # this takes something like 15 min and will require roughly 1 gb on disk
+    create_dwd_climate_summary_duckdb_dump(filepath, test=test)
+    con = duckdb.connect(str(filepath))
+    df_stations = con.execute("SELECT * FROM stations;").pl()
+    print(df_stations)
+    df_values = con.execute("SELECT * FROM values").pl()
+    print(df_values)
+
+
+if __name__ == "__main__":
+    filepath = ROOT / "dwd_obs_daily_climate_summary.duckdb"
+    test = "PYTEST_CURRENT_TEST" in os.environ
+    main(filepath, test=test)
