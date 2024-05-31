d3475da5243e6b6e3bce0e8b40f887ba35d550a2
Benjamin Gutzmann <gutzemann@gmail.com>
2024-06-01 01:58:32 +0200

    Restapi: Unset background colour

--- wetterdienst/ui/restapi.py
+++ wetterdienst/ui/restapi.py
@@ -73,7 +73,6 @@ def index():
                     font-family: Arial, sans-serif;
                     margin: 0;
                     padding: 0;
-                    background-color: #f0f0f0;
                 }}
 
                 header {{
