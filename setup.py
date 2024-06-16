5d0a719a1b957e2aed91b9f048e115dc3ee1be95
Andreas Motl <andreas.motl@panodata.org>
2024-06-16 23:30:04 +0200

    Revert "Remove unnecessary setup.py"
    This reverts commit 8703eb9d9e76e012c5e2ebeafcca7248057fbe3e.
    
    A surrogate `setup.py` is needed to re-enable the "Used by" section on GitHub.
    GitHub apparently still can't decode projects exclusively using
    `pyproject.toml` files in 2024.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2021, earthobservations developers.
+# Distributed under the MIT License. See LICENSE for more info.
+
+# This is a shim to hopefully allow GitHub to detect the package.
+# The build is done with Poetry.
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(name="wetterdienst")
