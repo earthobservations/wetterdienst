308b4db87fb8d13469f9f7f5114a587723fad2ab
Benjamin Gutzmann <gutzemann@gmail.com>
2024-12-30 09:41:56 +0100

    Separate cli tests

--- tests/ui/cli/test_cli.py
+++ tests/ui/cli/test_cli.py
@@ -2,7 +2,6 @@
 # Distributed under the MIT License. See LICENSE for more info.
 import json
 
-import pytest
 from click.testing import CliRunner
 
 from wetterdienst.ui.cli import cli
@@ -103,104 +102,6 @@ def test_coverage_dataset_climate_summary():
     assert response["annual"].keys() == {"climate_summary"}
 
 
-@pytest.mark.remote
-def test_cli_summarize():
-    runner = CliRunner()
-    result = runner.invoke(
-        cli,
-        "summarize --provider=dwd --network=observation "
-        "--parameters=daily/climate_summary/temperature_air_mean_2m "
-        "--station=00071 --date=1986-10-31/1986-11-01 --format=json",
-    )
-    if result.exit_code != 0:
-        raise ChildProcessError(result.stderr)
-    response = json.loads(result.stdout)
-    assert response.keys() == {"values"}
-    assert response["values"] == [
-        {
-            "station_id": "a87291a8",
-            "parameter": "temperature_air_mean_2m",
-            "date": "1986-10-31T00:00:00+00:00",
-            "value": 279.75,
-            "distance": 6.97,
-            "taken_station_id": "00072",
-        },
-        {
-            "station_id": "a87291a8",
-            "parameter": "temperature_air_mean_2m",
-            "date": "1986-11-01T00:00:00+00:00",
-            "value": 281.85,
-            "distance": 0.0,
-            "taken_station_id": "00071",
-        },
-    ]
-
-
-@pytest.mark.remote
-def test_cli_summarize_geojson():
-    runner = CliRunner()
-    result = runner.invoke(
-        cli,
-        "summarize --provider=dwd --network=observation "
-        "--parameters=daily/climate_summary/temperature_air_mean_2m "
-        "--station=00071 --date=1986-10-31/1986-11-01 --format=geojson",
-    )
-    if result.exit_code != 0:
-        raise ChildProcessError(result.stderr)
-    response = json.loads(result.stdout)
-    assert response.keys() == {"data"}
-    assert response["data"] == {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "properties": {"id": "a87291a8", "name": "summary(48.2156,8.9784)"},
-                "geometry": {"type": "Point", "coordinates": [8.9784, 48.2156]},
-                "stations": [
-                    {
-                        "station_id": "00071",
-                        "start_date": "1986-11-01T00:00:00+00:00",
-                        "end_date": "2019-12-31T00:00:00+00:00",
-                        "latitude": 48.2156,
-                        "longitude": 8.9784,
-                        "height": 759.0,
-                        "name": "Albstadt-Badkap",
-                        "state": "Baden-Württemberg",
-                    },
-                    {
-                        "station_id": "00072",
-                        "start_date": "1978-09-01T00:00:00+00:00",
-                        "end_date": "1995-05-31T00:00:00+00:00",
-                        "latitude": 48.2766,
-                        "longitude": 9.0001,
-                        "height": 794.0,
-                        "name": "Albstadt-Onstmettingen",
-                        "state": "Baden-Württemberg",
-                    },
-                ],
-                "values": [
-                    {
-                        "station_id": "a87291a8",
-                        "parameter": "temperature_air_mean_2m",
-                        "date": "1986-10-31T00:00:00+00:00",
-                        "value": 279.75,
-                        "distance": 6.97,
-                        "taken_station_id": "00072",
-                    },
-                    {
-                        "station_id": "a87291a8",
-                        "parameter": "temperature_air_mean_2m",
-                        "date": "1986-11-01T00:00:00+00:00",
-                        "value": 281.85,
-                        "distance": 0.0,
-                        "taken_station_id": "00071",
-                    },
-                ],
-            },
-        ],
-    }
-
-
 def test_cli_radar_stations_opera():
     runner = CliRunner()
     result = runner.invoke(cli, "radar --odim-code=ukdea")
@@ -215,10 +116,3 @@ def test_cli_radar_stations_dwd():
     response = json.loads(result.output)
     assert isinstance(response, list)
     assert len(response) == 20
-
-
-def test_cli_stripes():
-    runner = CliRunner()
-    result = runner.invoke(cli, "stripes --help")
-    assert result.exit_code == 0
-    assert "Commands:\n  interactive\n  stations\n  values\n" in result.output
