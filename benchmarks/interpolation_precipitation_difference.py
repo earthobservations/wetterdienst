d454ca337f0dd2c10228ddccba5cfa9deb817f9b
Benjamin Gutzmann <gutzemann@gmail.com>
2025-01-24 17:44:19 +0100

    Fix benchmark code

--- benchmarks/interpolation_precipitation_difference.py
+++ benchmarks/interpolation_precipitation_difference.py
@@ -1,32 +1,28 @@
 # Copyright (C) 2018-2023, earthobservations developers.
 # Distributed under the MIT License. See LICENSE for more info.
-from datetime import datetime
+import datetime as dt
 
 import polars as pl
 
-from wetterdienst import Parameter
 from wetterdienst.provider.dwd.observation import (
     DwdObservationRequest,
-    DwdObservationResolution,
 )
 
 LATLON = (52.52, 13.40)
 
 
-def get_interpolated_df(start_date: datetime, end_date: datetime) -> pl.DataFrame:
+def get_interpolated_df(start_date: dt.datetime, end_date: dt.datetime) -> pl.DataFrame:
     stations = DwdObservationRequest(
-        parameters=Parameter.PRECIPITATION_HEIGHT,
-        resolution=DwdObservationResolution.DAILY,
+        parameters=[("daily", "climate_summary", "precipitation_height")],
         start_date=start_date,
         end_date=end_date,
     )
     return stations.interpolate(latlon=LATLON).df
 
 
-def get_regular_df(start_date: datetime, end_date: datetime, exclude_stations: list) -> pl.DataFrame:
+def get_regular_df(start_date: dt.datetime, end_date: dt.datetime, exclude_stations: list) -> pl.DataFrame:
     stations = DwdObservationRequest(
-        parameters=Parameter.PRECIPITATION_HEIGHT.name,
-        resolution=DwdObservationResolution.DAILY,
+        parameters=[("daily", "climate_summary", "precipitation_height")],
         start_date=start_date,
         end_date=end_date,
     )
@@ -46,8 +42,8 @@ def calculate_percentage_difference(df: pl.DataFrame, text: str = "") -> float:
 
 
 def main():
-    start_date = datetime(2021, 1, 1)
-    end_date = datetime(2022, 1, 1)
+    start_date = dt.datetime(2021, 1, 1)
+    end_date = dt.datetime(2022, 1, 1)
     interpolated_df = get_interpolated_df(start_date, end_date)
     print(interpolated_df)
     exclude_stations = interpolated_df.get_column("taken_station_ids")[0]
