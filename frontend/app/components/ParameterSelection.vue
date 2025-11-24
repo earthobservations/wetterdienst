cab115303f60ec97437d45f29681b354e13af42a
Benjamin Gutzmann <benjamin.gutzmann@pm.me>
2025-11-24 12:30:28 +0100

    WIP

--- frontend/app/components/ParameterSelection.vue
+++ frontend/app/components/ParameterSelection.vue
@@ -1,95 +1,184 @@
 <script setup lang="ts">
-interface Selection {
-  provider: string | undefined
-  network: string | undefined
-  resolution: string | undefined
-  dataset: string | undefined
-  parameters: string[]
-}
 
-const props = defineProps<{ modelValue: Selection }>()
-const emit = defineEmits<{ (e: 'update:modelValue', v: Selection): void }>()
+// STATE
+const provider = useState<string | null>("provider", () => null)
+const network = useState<string | null>("network", () => null)
+const resolution = useState<string | null>("resolution", () => null)
+const dataset = useState<string | null>("dataset", () => null)
+const parameters = useState<string[]>("parameters", () => [])
 
-// local mutable copy
-const selection = reactive<Selection>({ ...props.modelValue })
-
-watch(() => props.modelValue, (v) => Object.assign(selection, v))
-
-function pushUpdate() {
-  emit('update:modelValue', { ...selection })
-}
-
-function setSelection<K extends keyof Selection>(key: K, value: Selection[K]) {
-  if (key === 'provider' && selection.provider !== value) {
-    selection.provider = value
-    selection.network = undefined
-    selection.resolution = undefined
-    selection.dataset = undefined
-    selection.parameters = []
-  } else if (key === 'network' && selection.network !== value) {
-    selection.network = value
-    selection.resolution = undefined
-    selection.dataset = undefined
-    selection.parameters = []
-  } else if (key === 'resolution' && selection.resolution !== value) {
-    selection.resolution = value
-    selection.dataset = undefined
-    selection.parameters = []
-  } else if (key === 'dataset' && selection.dataset !== value) {
-    selection.dataset = value
-    selection.parameters = []
-  }
-  pushUpdate()
-}
-
+// fetch coverage data
 const {data: coverage} = await useFetch('/api/coverage')
 
+// EXPECTING
 const providers = computed(() => coverage.value ? Object.keys(coverage.value).sort() : [])
 const networks = computed<string[]>(() => {
-  if (!coverage.value || !selection.provider) return []
-  return coverage.value[selection.provider] ?? []
+  if (!provider.value) return []
+  return coverage.value[provider.value] ?? []
 })
-const providerCoverageUrl = computed(() => {
-  if (!selection.provider || !selection.network) return null
-  return `/api/coverage?provider=${selection.provider}&network=${selection.network}`
-})
-const { data: providerCoverage, refresh: refreshProviderCoverage } = await useFetch(
-  providerCoverageUrl,
-  { immediate: false, watch: false }
+
+// provider-network coverage
+const { data: providerNetworkCoverage, refresh: refreshNetworkProviderCoverage } = await useFetch(
+  "/api/coverage",
+  {
+    query: {
+      provider: provider.value,
+      network: network.value
+    },
+    immediate: false,
+  }
 )
 
-watch(providerCoverageUrl, (url) => {
-  if (url) {
-    refreshProviderCoverage()
-  }
-})
-
 const resolutions = computed((): string[] => {
-  return providerCoverage.value ? Object.keys(providerCoverage.value) : []
+  return providerNetworkCoverage.value ? Object.keys(providerNetworkCoverage.value) : []
 })
 const datasets = computed((): string[] => {
-  if (!providerCoverage.value || !selection.resolution) return []
-  return Object.keys(providerCoverage.value[selection.resolution]).sort()
+  if (!providerNetworkCoverage.value || !resolution.value) return []
+  return Object.keys(providerNetworkCoverage.value[resolution.value]).sort()
 })
-
-const parameters = computed<string[]>(() => {
-  if (!providerCoverage.value || !selection.resolution || !selection.dataset) return []
-  return providerCoverage.value[selection.resolution][selection.dataset]
+const params = computed<string[]>(() => {
+  if (!providerNetworkCoverage.value || !resolution.value || !dataset.value) return []
+  return providerNetworkCoverage.value[resolution.value][dataset.value]
     .map((p: any) => p.name)
     .sort()
 })
 
-watch(parameters, (newParameters) => {
-  selection.parameters = [...newParameters]
-  pushUpdate()
+// watch to reset provider and network
+watch(provider, () => {
+  network.value = undefined
+  resolution.value = undefined
+  dataset.value = undefined
+  parameters.value = []
 })
+
+watch(network, (newValue) => {
+  resolution.value = undefined
+  dataset.value = undefined
+  parameters.value = []
+  // Only fetch when network is being set to a value (not when being cleared)
+  if (newValue && provider.value) {
+    refreshNetworkProviderCoverage()
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+watch([provider, network, resolution, dataset, parameters], () => {
+  emit('update:modelValue', {
+    provider: provider.value,
+    network: network.value,
+    resolution: resolution.value,
+    dataset: dataset.value,
+    parameters: parameters.value
+  })
+})
+
+watch(parameters, (newParameters) => {
+  parameters.value = [...newParameters]
+})
+
+
+// import type {ParameterSelection} from "~/types/parameter-selection-state.type";
+
+// const props = defineProps<{ modelValue: ParameterSelection }>()
+// const emit = defineEmits<{ (e: 'update:modelValue', v: ParameterSelection): void }>()
+//
+// // local mutable copy
+// const parameterSelection = reactive<ParameterSelection>({ ...props.modelValue })
+//
+// let isUpdating = false
+//
+// watch(() => props.modelValue, (v) => {
+//   if (isUpdating) return
+//   Object.assign(parameterSelection, v)
+// })
+//
+// function pushUpdate() {
+//   emit('update:modelValue', { ...parameterSelection })
+// }
+
+// const providers = computed(() => coverage.value ? Object.keys(coverage.value).sort() : [])
+// const networks = computed<string[]>(() => {
+//   if (!coverage.value || !parameterSelection.provider) return []
+//   return coverage.value[parameterSelection.provider] ?? []
+// })
+//
+// const { data: providerCoverage, refresh: refreshProviderCoverage } = await useFetch(
+//   "/api/coverage",
+//   {
+//     query: {w
+//       provider: parameterSelection.provider,
+//       network: parameterSelection.network
+//     },
+//     immediate: false,
+//     watch: false
+//   }
+// )
+//
+// const resolutions = computed((): string[] => {
+//   return providerCoverage.value ? Object.keys(providerCoverage.value) : []
+// })
+// const datasets = computed((): string[] => {
+//   if (!providerCoverage.value || !parameterSelection.resolution) return []
+//   return Object.keys(providerCoverage.value[parameterSelection.resolution]).sort()
+// })
+//
+// const parameters = computed<string[]>(() => {
+//   if (!providerCoverage.value || !parameterSelection.resolution || !parameterSelection.dataset) return []
+//   return providerCoverage.value[parameterSelection.resolution][parameterSelection.dataset]
+//     .map((p: any) => p.name)
+//     .sort()
+// })
+//
+// watch(() => parameterSelection.provider, () => {
+//   isUpdating = true
+//   parameterSelection.network = undefined
+//   parameterSelection.resolution = undefined
+//   parameterSelection.dataset = undefined
+//   parameterSelection.parameters = []
+//   providerCoverage.value = null
+//   pushUpdate()
+//   nextTick(() => isUpdating = false)
+// })
+//
+// watch(() => parameterSelection.network, (newValue) => {
+//   isUpdating = true
+//   parameterSelection.resolution = undefined
+//   parameterSelection.dataset = undefined
+//   parameterSelection.parameters = []
+//   pushUpdate()
+//   nextTick(() => isUpdating = false)
+//   // Only fetch when network is being set to a value (not when being cleared)
+//   if (newValue && parameterSelection.provider) {
+//     refreshProviderCoverage()
+//   }
+// })
+//
+// watch(() => parameterSelection.resolution, () => {
+//   isUpdating = true
+//   parameterSelection.dataset = undefined
+//   parameterSelection.parameters = []
+//   pushUpdate()
+//   nextTick(() => isUpdating = false)
+// })
+//
+// watch(() => parameterSelection.dataset, () => {
+//   isUpdating = true
+//   parameterSelection.parameters = []
+//   pushUpdate()
+//   nextTick(() => isUpdating = false)
+// })
+//
+
 
 </script>
 
 <template>
-  <USelect :model-value="selection.provider" :items="providers" placeholder="Select provider" @update:model-value="setSelection('provider', $event)"/>
-  <USelect :model-value= "selection.network" :items="networks" placeholder="Select network" @update:model-value="setSelection('network', $event)" :disabled="!selection.provider"/>
-  <USelect :model-value="selection.resolution" :items="resolutions" placeholder="Select resolution" @update:model-value="setSelection('resolution', $event)" :disabled="!selection.network"/>
-  <USelect :model-value="selection.dataset" :items="datasets" placeholder="Select dataset" @update:model-value="setSelection('dataset', $event)" :disabled="!selection.resolution"/>
-  <USelect :model-value="selection.parameters" :items="parameters" multiple placeholder="Select parameters" @update:model-value="(v) => { selection.parameters = v; pushUpdate() }" :disabled="!selection.dataset"/>
+<div class="flex flex-col gap-4">
+  <USelect :model-value="provider" :items="providers" placeholder="Select provider"/>
+  <USelect :model-value= "network" :items="networks" placeholder="Select network" :disabled="!provider" />
+  <USelect :model-value="resolution" :items="resolutions" placeholder="Select resolution" :disabled="!network" />
+  <USelect :model-value="dataset" :items="datasets" placeholder="Select dataset" :disabled="!resolution" />
+  <USelect :model-value="parameters" :items="parameters" multiple placeholder="Select parameters" :disabled="!dataset"/>
+</div>
 </template>
